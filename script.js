01020b4badbcdea8bfcd9721ac43d1a5c34f4447
Aman Satnami <54504150+amansatnami@users.noreply.github.com>
2026-01-01 20:32:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("amansatnami.github.io loaded");
+const toggle = document.getElementById("themeToggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark");
+  toggle.textContent = "☀️ Light Mode";
+}
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggle.textContent = "☀️ Light Mode";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggle.textContent = "🌙 Dark Mode";
+  }
+});
